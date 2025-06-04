a92c9504301b2eb8530987993d5fe3a6a408a277
Racha Al Kazzi <rachakazzi@gmail.com>
2025-06-04 17:29:31 +0300

    Navbar adds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_03_135604) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_03_132629) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,7 +25,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_03_135604) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "system_prompt"
   end
 
   create_table "messages", force: :cascade do |t|
